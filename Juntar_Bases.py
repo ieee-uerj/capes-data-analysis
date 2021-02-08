b0b42be6c39027f6e0eaf69345a944e0cf25b2da
Gustavo Marzullo <gustavmarzullo@gmail.com>
2021-02-08 20:55:24 -0300

    .

--- Juntar_Bases.py
+++ Juntar_Bases.py
@@ -2,18 +2,21 @@ import os
 import glob
 import pandas as pd
 
+print('Importando as bases')
 path = "/home/gustavo/Downloads/Bases para juntar - Discentes" #pasta onde as bases estão guardadas (no meu caso, é claro)
 
 all_files = glob.glob(os.path.join(path, "br-capes*.csv")) #"br-capes*.csv" é o início dos arquivos. Todos com "br-capes" serão juntados. ".csv" é a extensão
 
 df_from_each_file = (pd.read_csv(f, sep=';',encoding='latin1') for f in all_files) #aqui é importante dar o comando pd.read_csv de forma certa. Na maioria dos artigos da internet acaba-se usando como boilerplate não se percebe que tem que definir o sep e o encoding "brasileiros"
 
+print('Juntando as bases')
 df_merged = pd.concat(df_from_each_file, ignore_index=True) 
 
 #colunas inúteis para serem removidas do dataset são definidas aqui
 
+print('Ajeitando o dataset')
 colunas_para_apagar=['CD_ENTIDADE_EMEC',
-'NM_ENTIDADE_ENSINO','NM_MODALIDADE_PROGRAMA','CD_CONCEITO_CURSO'
+'NM_ENTIDADE_ENSINO','NM_MODALIDADE_PROGRAMA','CD_CONCEITO_CURSO',
 'NM_GRAU_PROGRAMA','NM_PROGRAMA_IES',
 'ID_PESSOA','TP_DOCUMENTO_DISCENTE','NR_DOCUMENTO_DISCENTE','NM_DISCENTE','DS_FAIXA_ETARIA'
 ,'DT_MATRICULA_DISCENTE',
@@ -46,6 +49,7 @@ df_merged.CD_CONCEITO_PROGRAMA=df_merged.CD_CONCEITO_PROGRAMA.astype('int64')
 df_merged.index=range(0,len(df_merged))
 ###########################################################################
 
+print('Salvando o arquivo')
 #salvando o csv
 df_merged.to_csv('Discentes_2013-2019.csv',encoding='latin1',index=False)
 
